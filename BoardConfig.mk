33b7eb4b85e1411cbf6a6e05ab6e1032d4e8c0ff
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-05-22 02:13:57 +0200

    [WIP] initial Tablet Z Wifi support
    To be fixed:
    - volume node (not event4 and event6)
    - signature
    
    Change-Id: I7930d13fc3ab25f9e5f7fc2a193106c92a36ea83

---
+++ BoardConfig.mk
@@ -0,0 +1,24 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from the common pollux definitions
+-include device/sony/pollux-common/BoardConfigCommon.mk
+
+# inherit from the proprietary version
+-include vendor/sony/pollux_windy/BoardConfigVendor.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := SGP311,SGP312,pollux_windy
+
+TARGET_KERNEL_CONFIG := cm_fusion3_pollux_windy_defconfig
---
+++ cm.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/sony/pollux_windy/full_pollux_windy.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1200
+TARGET_SCREEN_WIDTH := 1920
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321_1275-5841/SGP321:4.1.2/10.1.1.A.1.253/Avt3rw:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 4.1.2 10.1.1.A.1.253 Avt3rw test-keys"
+
+PRODUCT_NAME := cm_pollux_windy
+PRODUCT_DEVICE := pollux_windy
