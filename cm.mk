2df7188343e58734b43e33f617c0ec3f79914451
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-05-22 02:13:57 +0200

    [WIP] initial Tablet Z Wifi support
    Change-Id: I7930d13fc3ab25f9e5f7fc2a193106c92a36ea83

---
+++ cm.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/sony/pollux_windy/full_pollux_windy.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1200
+TARGET_SCREEN_WIDTH := 1920
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP311 BUILD_FINGERPRINT=Sony/SGP311_1272-1907/SGP311:4.1.2/10.1.C.0.344/p35_rw:user/release-keys PRIVATE_BUILD_DESC="SGP311-user 4.1.2 10.1.C.0.344 p35_rw test-keys"
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+PRODUCT_NAME := cm_pollux_windy
+PRODUCT_DEVICE := pollux_windy
