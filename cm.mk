aba32ea3cd6c6c9c41150862984a4a25c7f8d061
Chirayu Desai <cdesai@cyanogenmod.org>
2013-06-07 11:32:35 +0530

    relocate TARGET_SCREEN_{WIDTH,HEIGHT}
    Change-Id: I40107b9d91fc87c59950c35f3fcab612e45585e6

--- cm.mk
+++ cm.mk
@@ -3,10 +3,6 @@ $(call inherit-product, device/sony/pollux_windy/full_pollux_windy.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Boot Animation
-TARGET_SCREEN_HEIGHT := 1200
-TARGET_SCREEN_WIDTH := 1920
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP311 BUILD_FINGERPRINT=Sony/SGP311_1272-1907/SGP311:4.1.2/10.1.C.0.344/p35_rw:user/release-keys PRIVATE_BUILD_DESC="SGP311-user 4.1.2 10.1.C.0.344 p35_rw test-keys"
 
 # Inherit some common CM stuff.
