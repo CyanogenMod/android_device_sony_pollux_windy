92c096aa9d7da6301a9f0eeff6e2a0fb1e136a0e
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-06-09 08:39:19 +0200

    pollux_windy: relocate TARGET_SCREEN_{WIDTH,HEIGHT}
    Change-Id: I9d2e3eb85571b9d05db28b6113706413d875b9d5

--- full_pollux_windy.mk
+++ full_pollux_windy.mk
@@ -22,6 +22,9 @@
 #
 #
 
+TARGET_SCREEN_HEIGHT := 1200
+TARGET_SCREEN_WIDTH := 1920
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
  
