bb2e0e57e3b969c5369ebc795ff09c1a76390efd
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-06-22 17:10:46 +0200

    pollux_windy: disable radio stuff
    Change-Id: Id9321b983be2af3e9fbca57b49e3f97a9354565b

--- full_pollux_windy.mk
+++ full_pollux_windy.mk
@@ -25,6 +25,9 @@
 TARGET_SCREEN_HEIGHT := 1200
 TARGET_SCREEN_WIDTH := 1920
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carrier=wifi-only
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
  
