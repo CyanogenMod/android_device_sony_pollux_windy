51889befb7df43a6b921bf4c530333f79ffedc49
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-06-22 17:10:46 +0200

    pollux_windy: disable radio stuff
    Change-Id: Id9321b983be2af3e9fbca57b49e3f97a9354565b

--- pollux_windy.mk
+++ pollux_windy.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+BOARD_HAVE_RADIO := false
+
 # Inherit the pollux-common definitions
 $(call inherit-product, device/sony/pollux-common/pollux-common.mk)
 
