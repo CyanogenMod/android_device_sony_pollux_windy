f6c03a2cbbfe25bce61ab1d29f7723bd259ebec5
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-10-20 16:31:52 +0200

    pollux_windy: use common resources
    Change-Id: I73c12a1c3166a9f7346817b8081582ecc73fe590

--- full_pollux_windy.mk
+++ full_pollux_windy.mk
@@ -32,7 +32,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
  
 # Common Sony Resources
-$(call inherit-product, device/sony/common/resources-xxhdpi.mk)
+$(call inherit-product, device/sony/common/resources.mk)
 
 # Inherit from pollux_windy device
 $(call inherit-product, device/sony/pollux_windy/pollux_windy.mk)
