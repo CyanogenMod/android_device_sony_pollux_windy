213f36e7040e6141d4e87cbe1e003a6b0d77f91d
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-07-08 12:30:33 +0200

    update build fingerprint
    Change-Id: I5c0439450154a079ac7b8f4b0b1200ebd8c3b0f6

--- cm.mk
+++ cm.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/sony/pollux_windy/full_pollux_windy.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP311 BUILD_FINGERPRINT=Sony/SGP311/SGP311:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="SGP311-user 4.4.4 10.5.1.A.0.283 5P53rQ test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP311 BUILD_FINGERPRINT=Sony/SGP311/SGP311:5.0.2/10.6.A.0.454/1088868577:user/release-keys PRIVATE_BUILD_DESC="SGP311-user 5.0.2 10.6.A.0.454 1088868577 test-keys"
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
